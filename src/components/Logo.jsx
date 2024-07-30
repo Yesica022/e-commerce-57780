69433f35f13d09c995915a393711f09f25c94901
Yesica Martinez <yesica.martinez@akui.solutions>
2024-07-29 20:39:57 -0500

    Cambios

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className='p-2'>
     <img
-      src="src/img/Logo.V1.svg"
+      src="/Logo.V1.svg"
       alt="Serendipia"
       className="justify-start w-44 h-16"
     />
